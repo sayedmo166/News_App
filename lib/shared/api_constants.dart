5b37023a20ebd0d0836b23a55cdfddb16ae75229
HP <sm4679313@gmail.com>
2024-09-21 22:32:09 +0300

    search bar

--- lib/shared/api_constants.dart
+++ lib/shared/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiContants {
   static const String baseurl = 'newsapi.org';
-  static const String key = 'bf20635e54eb484c8b3be488349d152e';
+  static const String key = '0f3a900de2ce4d0786ef4a75d7d07410';
   static const String sourcesendpoint = '/v2/top-headlines/sources';
   static const String newsendpoint = '/v2/top-headlines';
 }
